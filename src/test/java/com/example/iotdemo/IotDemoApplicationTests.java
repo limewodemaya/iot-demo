eac11fb586edbe2e2d7b3dab4bc1ffe9e6d1576a
Huang <2498844026@qq.com>
2026-05-14 15:17:28 +0800

    init spring boot project

---
+++ src/test/java/com/example/iotdemo/IotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
